345bc0113c687431c724367c2f48f8a51ddc200d
gomachan <go_nakanishi@voyagegroup.com>
2014-06-22 09:19:50 +0900

    クラス定義を別ファイルに切り分けた

---
+++ class_for_no_di.php
@@ -0,0 +1,63 @@
+<?php
+
+class Song
+{
+    public function __construct($title=null)
+    {
+        $this->title = $title;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
+
+class MusicPlayer
+{
+    private $song;
+
+    public function __construct()
+    {
+        $this->song = new Song('test');
+    }
+
+    public function play()
+    {
+        echo '"'.$this->song->getTitle().'"を再生するよ';
+    }
+}
+
+class TwitterClient
+{
+    public function post()
+    {
+        // 投稿処理
+        echo '音ゲーをプレイしたよとつぶやくよ';
+    }
+}
+
+class Otoge
+{
+    private $song;
+    private $music_player;
+    private $twitter_client;
+
+    public function __construct()
+    {
+        $this->song = new Song('test');
+        $this->music_player = new MusicPlayer();
+        $this->twitter_client = new TwitterClient();
+    }
+
+    public function play()
+    {
+        echo '"'.$this->song->getTitle().'"で音ゲーを開始するよ';
+        $this->music_player->play();
+        // その他得点記録したりとかの処理
+    }
+
+    public function tweet()
+    {
+        $this->twitter_client->post();
+    }
+}
