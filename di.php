b651ed36246f433d8bef3723ac8ff5f02077758f
gomachan <go_nakanishi@voyagegroup.com>
2014-06-22 09:03:16 +0900

    DIした版にツイート機能をつけた

--- di.php
+++ di.php
@@ -1,4 +1,17 @@
 <?php
+interface TwitterClientInterface
+{
+    public function post();
+}
+class TwitterClient implements TwitterClientInterface
+{
+    public function post()
+    {
+        // 投稿処理
+        echo '音ゲーをプレイしたよとつぶやくよ';
+    }
+}
+
 interface SongInterface
 {
     public function getTitle();
@@ -54,10 +67,11 @@ class Otoge
      */
     private $music_player;
 
-    public function __construct(SongInterface $song, MusicPlayerInterface $music_player)
+    public function __construct(SongInterface $song, MusicPlayerInterface $music_player, TwitterClientInterface $twitter_client)
     {
         $this->song = $song;
         $this->music_player = $music_player;
+        $this->twitter_client = $twitter_client;
     }
 
     public function play()
@@ -65,9 +79,15 @@ class Otoge
         echo '"'.$this->song->getTitle().'"で音ゲーを開始するよ';
         $this->music_player->play();
     }
+    public function tweet()
+    {
+        $this->twitter_client->post();
+    }
 }
 
 $song = new Song('test');
 $music_player = new MusicPlayer($song);
-$otoge = new Otoge($song, $music_player);
+$twitter_client = new TwitterClient();
+$otoge = new Otoge($song, $music_player, $twitter_client);
 $otoge->play();
+$otoge->tweet();
